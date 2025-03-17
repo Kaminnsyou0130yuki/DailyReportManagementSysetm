800ac3899e5c1dd219fa5dde79767f71d8e9b224
Kaminnsyou0130yuki <min0130narukami@gmail.com>
2025-03-18 03:03:38 +0900

    日報一覧
    氏名・日付・タイトルの表示　動作確認済み
    登録画面への画面遷移　動作確認済み
    
    詳細画面への画面遷移　未実装

--- src/main/java/com/techacademy/controller/DailyReportController.java
+++ src/main/java/com/techacademy/controller/DailyReportController.java
@@ -1,31 +1,34 @@
 package com.techacademy.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.techacademy.service.DailyReportService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.techacademy.repository.EmployeeRepository;
-import com.techacademy.service.EmployeeService;
-
 @Controller
 @RequestMapping("reports")
 public class DailyReportController {
 
-    private final EmployeeRepository employeeRepository;
-    private final EmployeeService employeeService;
+    private final DailyReportService dailyReportService;
 
-    @Autowired
-    public DailyReportController(EmployeeService employeeService, EmployeeRepository employeeRepository) {
-        this.employeeService = employeeService;
-        this.employeeRepository = employeeRepository;
+    DailyReportController(DailyReportService dailyReportService) {
+        this.dailyReportService = dailyReportService;
     }
 
 //    日報一覧画面
     @GetMapping
     public String list(Model model) {
+
+        model.addAttribute("listSize", dailyReportService.findAll().size());
+        model.addAttribute("dairyReportList", dailyReportService.findAll());
         return "dailyReport/dailyReportList";
+
+    }
+
+    @GetMapping("/add")
+    public String create(Model model) {
+        return "dailyReport/dailyReportNew";
     }
 
 }
---
+++ src/main/java/com/techacademy/service/DailyReportService.java
@@ -0,0 +1,28 @@
+package com.techacademy.service;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.techacademy.entity.DailyReport;
+import com.techacademy.entity.Employee;
+import com.techacademy.repository.DailyReportRepository;
+
+@Service
+public class DailyReportService {
+
+    private final DailyReportRepository dailyReportRepository;
+
+    public DailyReportService(DailyReportRepository dailyReportRepository) {
+        this.dailyReportRepository = dailyReportRepository;
+    }
+
+//    日報一覧表示
+    public List<DailyReport> findAll() {
+        return dailyReportRepository.findAll();
+    }
+
+//    入力チェックとかもここでやるって感じかな？？
+
+}

0b5386ad2b1df69c6b2e4899b1b43e362230c6b8
Kaminnsyou0130yuki <min0130narukami@gmail.com>
2025-03-18 05:58:08 +0900

    不要なインポートの削除

--- src/main/java/com/techacademy/service/DailyReportService.java
+++ src/main/java/com/techacademy/service/DailyReportService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.techacademy.entity.DailyReport;
-import com.techacademy.entity.Employee;
 import com.techacademy.repository.DailyReportRepository;
 
 @Service

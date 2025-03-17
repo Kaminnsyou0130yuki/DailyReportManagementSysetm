5d6d8e0c5b9f05b4ffa92a78b2d1d04a1af84e2b
Kaminnsyou0130yuki <min0130narukami@gmail.com>
2025-03-18 00:54:04 +0900

    日報　エンティティクラス作成

---
+++ src/main/java/com/techacademy/entity/DailyReport.java
@@ -0,0 +1,57 @@
+package com.techacademy.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "reports")
+public class DailyReport {
+
+//  ID
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+//    日付
+    @Column(nullable = false)
+    private LocalDate reportDate;
+
+//    タイトル
+    @Column(length = 100, nullable = false)
+    private String title;
+
+//    内容
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
+    private String content;
+
+//    社員番号
+    @ManyToOne
+    @JoinColumn(name = "employee_code", referencedColumnName = "code")
+    private Employee employee;
+
+//    削除フラグ
+    @Column(nullable = false)
+    private boolean deleteFlg;
+
+//    登録日時
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+//    更新日時
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}

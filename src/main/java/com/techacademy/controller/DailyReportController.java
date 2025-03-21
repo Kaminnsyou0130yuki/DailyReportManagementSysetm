c3bbf9697eca31dbe9a70364cc8ce313cff4aa2a
Kaminnsyou0130yuki <min0130narukami@gmail.com>
2025-03-21 20:44:16 +0900

    日報詳細画面　実装
    画面遷移　動作確認済み
    画面項目　表示確認済み
    戻るボタン押下で日報一覧表示画面への遷移　動作確認済み
    新規日報登録後、日報の詳細の画面項目　表示確認済み
    新規ユーザー登録後、日報の詳細の画面項目　表示確認済み
    
    更新ボタン押下時の画面遷移　未実装
    削除ボタン押下時の画面遷移　未実装

--- src/main/java/com/techacademy/controller/DailyReportController.java
+++ src/main/java/com/techacademy/controller/DailyReportController.java
@@ -4,6 +4,7 @@ import com.techacademy.constants.ErrorKinds;
 import com.techacademy.constants.ErrorMessage;
 import com.techacademy.entity.DailyReport;
 import com.techacademy.entity.Employee;
+import com.techacademy.repository.DailyReportRepository;
 import com.techacademy.service.DailyReportService;
 import com.techacademy.service.EmployeeService;
 
@@ -16,6 +17,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,17 +25,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("reports")
 public class DailyReportController {
 
+    private final DailyReportRepository dailyReportRepository;
+
     private final DailyReportService dailyReportService;
     private final EmployeeService employeeService;
 
     @Autowired
-    DailyReportController(DailyReportService dailyReportService, EmployeeService employeeService) {
+    DailyReportController(DailyReportService dailyReportService, EmployeeService employeeService,
+            DailyReportRepository dailyReportRepository) {
 
         this.dailyReportService = dailyReportService;
         this.employeeService = employeeService;
+        this.dailyReportRepository = dailyReportRepository;
     }
 
-//    日報一覧画面
+// 日報一覧画面
     @GetMapping
     public String list(Model model) {
 
@@ -42,49 +48,56 @@ public class DailyReportController {
         return "dailyReport/dailyReportList";
     }
 
-//    新規登録画面
+// 新規登録画面
     @GetMapping("/add")
     public String create(@ModelAttribute DailyReport dailyReport, Principal principal, Model model) {
-//        ログイン中のユーザの社員番号取得
+        // ログイン中のユーザの社員番号取得
         String code = principal.getName();
-//        ログイン中のユーザの社員番号を使用してレコード取得
+        // ログイン中のユーザの社員番号を使用してレコード取得
         Employee employee = employeeService.findByCode(code);
-//        dailyReportエンティティのemployeeにセット
+        // dailyReportエンティティのemployeeにセット
         dailyReport.setEmployee(employee);
         return "dailyReport/dailyReportNew";
     }
 
-//    新規登録処理
-//    入力チェックも実装しなきゃいけない。画面設計者に入力チェック仕様が記載されている
+// 新規登録処理
     @PostMapping("/add")
     public String add(@ModelAttribute @Validated DailyReport dailyReport, BindingResult res, Principal principal,
             Model model) {
 
-//        エラーがある場合、新規登録画面へ画面遷移
+        // エラーがある場合、新規登録画面へ画面遷移
         if (res.hasErrors()) {
             return create(dailyReport, principal, model);
         }
 
-//      login中のユーザ情報を取得
-//        getNameで社員番号を取得
+        // login中のユーザ情報を取得
+        // getNameで社員番号を取得
         String code = principal.getName();
-//      employeeに社員番号で検索したレコード（ログイン中の従業員の情報）を格納
+        // employeeに社員番号で検索したレコード（ログイン中の従業員の情報）を格納
         Employee employee = employeeService.findByCode(code);
 
-//        日報テーブルに　ログイン中のユーザかつ入力した日付　の日報データが存在する場合エラー
-//        ErrorMessagesクラスに専用のエラーメッセージあり
+        // 日報テーブルに ログイン中のユーザかつ入力した日付 の日報データが存在する場合エラー
+        // ErrorMessagesクラスに専用のエラーメッセージあり
         if (dailyReportService.existsReportByEmployeeAndDate(employee, dailyReport.getReportDate())) {
-//            エラー名、エラーメッセージ取得
+            // エラー名、エラーメッセージ取得
             String errorName = ErrorMessage.getErrorName(ErrorKinds.DATECHECK_ERROR);
             String errorValue = ErrorMessage.getErrorValue(ErrorKinds.DATECHECK_ERROR);
             model.addAttribute(errorName, errorValue);
             return create(dailyReport, principal, model);
         }
 
-//        dailyReportのemployeeにそのレコード(ログイン中の従業員の情報)を格納
+        // dailyReportのemployeeにそのレコード(ログイン中の従業員の情報)を格納
         dailyReport.setEmployee(employee);
-//        DBにデータを保存
+        // DBにデータを保存
         dailyReportService.save(dailyReport);
         return "redirect:/reports";
     }
+
+// 一件検索
+    @GetMapping("/{id}/")
+    public String detail(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("dailyReport", dailyReportService.findById(id));
+        return "dailyReport/dailyReportDetail";
+    }
+
 }
--- src/main/java/com/techacademy/service/DailyReportService.java
+++ src/main/java/com/techacademy/service/DailyReportService.java
@@ -1,15 +1,16 @@
 package com.techacademy.service;
 
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.techacademy.entity.DailyReport;
 import com.techacademy.entity.Employee;
 import com.techacademy.repository.DailyReportRepository;
+import com.techacademy.repository.EmployeeRepository;
 
 @Service
 public class DailyReportService {
@@ -20,12 +21,12 @@ public class DailyReportService {
         this.dailyReportRepository = dailyReportRepository;
     }
 
-//    日報一覧表示
+    // 日報一覧表示
     public List<DailyReport> findAll() {
         return dailyReportRepository.findAll();
     }
 
-//    日報保存
+    // 日報保存
     public void save(DailyReport dailyReport) {
 
         LocalDateTime now = LocalDateTime.now();
@@ -35,10 +36,16 @@ public class DailyReportService {
         dailyReportRepository.save(dailyReport);
     }
 
-//    employeeとreportDateが存在するかどうかチェック
-//    空かどうかを判定したいので、isEmptyがtrueの時にfalseを返すように実装
+    // employeeとreportDateが存在するかどうかチェック
+    // 空かどうかを判定したいので、isEmptyがtrueの時にfalseを返すように実装
     public boolean existsReportByEmployeeAndDate(Employee employee, LocalDate reportDate) {
         return !dailyReportRepository.findByEmployeeAndReportDate(employee, reportDate).isEmpty();
     }
 
+    // 一件検索
+    public DailyReport findById(Integer id) {
+        Optional<DailyReport> option = dailyReportRepository.findById(id);
+        DailyReport dailyReport = option.orElse(null);
+        return dailyReport;
+    }
 }
